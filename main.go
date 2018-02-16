ba3ab3ce49057d4f0f45b22dcdb035bc409db601
soichiro-matsumoto <soujjj@gmail.com>
2018-02-16 20:12:28 +0900

    wip

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"html/template"
+	"io"
+	"net/http"
 	"strconv"
 	"weather-api/config"
 	"weather-api/handlers"
@@ -14,10 +17,38 @@ var (
 	Config = config.NewConfig()
 )
 
+// TemplateRenderer is a custom html/template renderer for Echo framework
+type TemplateRenderer struct {
+	templates *template.Template
+}
+
+// Render renders a template document
+func (t *TemplateRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 
 	// Echoのインスタンス作る
 	e := echo.New()
+
+	renderer := &TemplateRenderer{
+		templates: template.Must(template.ParseGlob("resources/templates/*.html")),
+	}
+	e.Renderer = renderer
+
+	// Named route "foobar"
+	e.GET("", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "index.html", map[string]interface{}{
+			"name": "Dolly!",
+		})
+	})
 
 	// 全てのリクエストで差し込みたいミドルウェア（ログとか）はここ
 	e.Use(middleware.Logger())
